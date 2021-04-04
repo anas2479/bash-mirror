33252d6bea62b52cc847d30ec85816200e77cc79
Anas Mohamed <amohamed2479@gmail.com>
2021-04-04 14:00:29 -0500

    check for error

--- src/js/commands/help_command.js
+++ src/js/commands/help_command.js
@@ -5,11 +5,19 @@ const commands = require('./commands.json')
 module.exports = function help(userInput){
     
     let cmd = document.getElementById('cmdEnvironment');
-
-    commands.forEach((command)=>{
-       cmd.innerHTML += `
-            <p> <b>${command.name}</b> &nbsp ----> &nbsp <i>${command.useCase}</i></p>
-       `
-    })  
+    
+    
+    if(userInput.length > 1){// if the user wrote more than `help`
+        cmd.innerHTML += `
+        <span> <i class="fas fa-angle-right angle"></i>${_.join(userInput, ' ')}</span>
+        <p>Command doesn't exist yet!!</p>
+        `
+    }  else{// if the user only wrote `help`
+        commands.forEach((command)=>{
+            cmd.innerHTML += `
+                 <p> <b>${command.name}</b> &nbsp ----> &nbsp <i>${command.useCase}</i></p>
+            `
+         })
+    }
     
 }
