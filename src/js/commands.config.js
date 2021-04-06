87df8b8dd44ec36c7a1ba06b5b76534ec89677fe
Anas Mohamed <amohamed2479@gmail.com>
2021-04-06 08:35:39 -0500

    create a command config file 🐫

---
+++ src/js/commands.config.js
@@ -0,0 +1,37 @@
+// import all the commands
+const fileSystem = require('./filesystem')
+const help_command = require('./commands/help_command');
+const clear_command = require('./commands/clear-command');
+const test = require('./commands/test');
+
+
+
+
+module.exports = [
+    {
+        name:"help",
+        function:help_command
+    },
+
+    {
+        name:"clear",
+        function:clear_command
+    },
+
+    {
+        name:"mkdir",
+        function:fileSystem
+    },
+
+
+    {
+        name:"rmdir",
+        function:fileSystem
+    },
+
+    {
+        name:"dir",
+        function:fileSystem
+    }
+    
+]
