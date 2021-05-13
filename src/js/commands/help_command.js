050fa3dd876c0f05ffe14e9370e02f1124f24d9f
Anas Mohamed <amohamed2479@gmail.com>
2021-05-13 13:55:33 -0500

    clean up

--- src/js/commands/help_command.js
+++ src/js/commands/help_command.js
@@ -11,7 +11,7 @@ module.exports = function help(userInput){
     
     if(userInput.length > 1){// if the user wrote more than `help`
         commandOutputContainer.innerHTML += `
-        <span> <i class="fas fa-angle-right angle"></i>${_.join(userInput, ' ')}</span>
+        <span>${_.join(userInput, ' ')}</span>
         <p>Command doesn't exist yet!!</p>
         `
     }  else{// if the user only wrote `help`
