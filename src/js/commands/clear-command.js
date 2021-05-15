38a6b564f0dc8613be30f4754134576c79ac8fc6
Anas Mohamed <amohamed2479@gmail.com>
2021-05-15 12:41:57 -0500

    clean up

--- src/js/commands/clear-command.js
+++ src/js/commands/clear-command.js
@@ -3,14 +3,8 @@ const { contentContainer } = require("../bash.config")
 
 
 module.exports = function clear(userInput){
-
-    
-    
-    
-    
     contentContainer.innerHTML = `
     <span class="command-input-container">
-        <i class="fas fa-angle-right angle" aria-hidden="true"></i>
         <input autofocus="true">
     </span>
     `
