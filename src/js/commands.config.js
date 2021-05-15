9523f48a0d927ad0775457917f1de289ac15c505
Anas Mohamed <amohamed2479@gmail.com>
2021-05-15 15:46:45 -0500

    add a description to the commands

--- src/js/commands.config.js
+++ src/js/commands.config.js
@@ -10,44 +10,52 @@ const clear_command = require('./commands/clear-command');
 module.exports = [
     {
         name:"help",
-        function:help_command
+        function:help_command,
+        description: 'Lists all the existing commands. Used when you need help.⛑'
     },
 
     {
         name:"clear",
-        function:clear_command
+        function:clear_command,
+        description:'Clears everything on the screen. Used when you want to start fresh.✨'
     },
 
     {
         name:'cd',
-        function:fileSystem.cd
+        function:fileSystem.cd,
+        description:'Used to move though directories. ⏯'
 
     },
 
     {
         name:"mkdir",
-        function:fileSystem.mkdir
+        function:fileSystem.mkdir,
+        description:'Creates a new directory.📁'
     },
 
 
     {
         name:"rmdir",
-        function:fileSystem.rm
+        function:fileSystem.rm,
+        description:'Used to remove a directory. 🗑'
     },
 
     {
         name:"rm",
-        function:fileSystem.rm
+        function:fileSystem.rm,
+        description:'Used to remove a file. 🗑'
     },
 
     {
         name:"ls",
-        function:fileSystem.ls
+        function:fileSystem.ls,
+        description:'Lists all the content in the current directory.📃'
     },
 
     {
         name:"touch",
-        function:fileSystem.touch
+        function:fileSystem.touch,
+        description:'Creates a new file.🖨'
     }
     
 ]
