41bfe32394807ddc5d4007244581c2b70e12d200
Anas Mohamed <amohamed2479@gmail.com>
2021-05-16 19:12:59 -0500

    rewrite the file system
    1 - `rmdir` is it's own function now.
    
    2- all the functions that took an array as a parameter can now work with a string as well.

--- src/js/commands/file-system/filesystem.js
+++ src/js/commands/file-system/filesystem.js
@@ -1,288 +1,287 @@
+const _ = require("lodash");
+const { commandOutputContainer } = require("../../bash.config");
+const { error } = require("../../errors");
+const icons = require("../../icons");
+let allFolders = require("./folders");
 
-const _ = require('lodash')
-const { commandOutputContainer } = require('../../bash.config')
-const { error } = require('../../errors')
-const icons = require('../../icons')
-let allFolders = require('./folders')
-
-
-
-
-// the max words a folder name or file name can have
-let maxNameSize = 100
 
 /**
  * The current folder in the allFolders array
  */
-let currentFolder = allFolders[0]
+let currentFolder = allFolders[0];
 
-
-let history = []
+let history = [];
 
 // index will be used to track history.
-let index = -1
+let index = -1;
 
 /**
- * The function used to change through directories.
- * @param {Array} command The user input split into words.
+ * Used to change through directories.
+ * @param {string} command The folder in the current directory to change into.
  */
 
-function cd(command){
+function cd(command) {
 
-    // Extract the folder name from the command written by the user
-    let folderName = _.join(command.slice(1, maxNameSize), ' ')
-    
-    
-    if (folderName === '..'){// If the user typed `..` after `cd`
+  if (command.length >= 1) {
+    let folderName;
 
-        //check weather index is less than -1.
-        if(index <= -1){
-            index = -1
-        }else{
-            // set the current folder to the previous folder
-            currentFolder = history[index]
-            history.splice(index)
-            index --
-        }
-        
-
-        // console.log('current folder :',currentFolder)
-        // console.log('history', history)
-        // console.log('index', index)
-        
-    }else{
-
-        // Find a sub folder with a name that matches
-        // with the folder name specified by the user.
-        let match  = _.find(currentFolder.subfolders,{name:folderName})
-
-        // If a match was found, 
-        // set the current folder to the found match.
-        if(match != undefined){
-
-            let removed = _.remove(history, (folder)=>{
-                return folder.name === currentFolder.name
-            })
-            history.push(currentFolder)
-            index ++
-            currentFolder = match 
-
-             //console.log('current folder :',currentFolder)
-             //console.log('history', history)
-
-        }// Match was not found,
-        else{
-            error(`<b>${folderName}</b> was no where to be found 😟 `)
-        }
-
+    if (Array.isArray(command)) {
+      folderName = _.join(command, " ");
+    } else {
+      folderName = command;
     }
 
+    if (folderName === "..") {
+      // If the user typed `..` after `cd`
 
-    
-    
+      //check weather index is less than -1.
+      if (index <= -1) {
+        index = -1;
+      } else {
+        // set the current folder to the previous folder
+        currentFolder = history[index];
+        history.splice(index);
+        index--;
+      }
+      // Some debug code.
+      // console.log('current folder :',currentFolder)
+      // console.log('history', history)
+      // console.log('index', index)
+
+    } else {
+      // Find a sub folder with a name that matches
+      // with the folder name specified by the user.
+      let match = _.find(currentFolder.subfolders, { name: folderName });
+
+      // If a match was found,
+      // set the current folder to the found match.
+      if (match != undefined) {
+        let removed = _.remove(history, (folder) => {
+          return folder.name === currentFolder.name;
+        });
+        history.push(currentFolder);
+        index++;
+        currentFolder = match;
+
+        //console.log('current folder :',currentFolder)
+        //console.log('history', history)
+      } // Match was not found,
+      else {
+        error(`<b>${folderName}</b> was no where to be found 😟 `);
+      }
+    }
+  }else{
+      error('Folder to change into was not provided.😟')
+  }
 }
 
 /**
- * 
+ * Gets the current location in the file system
  * @returns {string} The current location in the file system.
  */
-function printPath(){
-    let path = []
+function printPath() {
+  let path = [];
 
-    history.forEach((folder)=>{
-        path.push(folder.name + '/')
-    })
-    path.push(currentFolder.name)
+  history.forEach((folder) => {
+    path.push(folder.name + "/");
+  });
+  path.push(currentFolder.name);
 
-    return _.join(path,'')
+  return _.join(path, "");
 }
-
-
 
 /**
- * Creates a new folder in the current directory.
- * @param {Array} command The user input split into words.
+ * Creates a new folder with the specified name in the current directory.
+ * @param {string} command The folder to create.
  */
-function mkdir(command){
+function mkdir(command) {
+  if (command.length >= 1) {
+    // user provided a folder name
+    let folderName;
 
-    if(command.length > 1){ // user provided folder name --> command = ['mkdir', 'folder name',]
-        let folderName = _.join(command.slice(1, maxNameSize), ' ')
-
-        let newFolder = {
-            name: folderName,
-            subfolders:[],
-            files:[]
-        }
-    
-        currentFolder.subfolders.push(newFolder)
-        commandOutputContainer.innerHTML += `<p>Created <b>✨${folderName}✨</b> directory.</p>`
-
-    }else{//user did not provide folder name
-        error(`Directory name was <b>not</b> provided. ☹`)
+    if (Array.isArray(command)) {
+      folderName = _.join(command, " ");
+    } else {
+      folderName = command;
     }
 
-}
+    let newFolder = {
+      name: folderName,
+      subfolders: [],
+      files: [],
+    };
 
+    currentFolder.subfolders.push(newFolder);
+    commandOutputContainer.innerHTML += `<p>Created <b>✨${folderName}✨</b> directory.</p>`;
+  } else {
+    //user did not provide folder name
+    error(`Directory name was <b>not</b> provided. ☹`);
+  }
+}
 
 /**
  * Returns all the contents inside the current directory in HTML list.
- * @param {Array} command The user input split into wards
  */
-function ls(command){
+function ls(command) {
+  if (command.length < 1) {
+    // make sure the user only wrote `ls` keyword
 
-    if (command.length === 1){// make sure the user only wrote `ls` keyword
+    // check if the current folder
+    // has any subfolders or files
+    if (currentFolder.subfolders.length > 0 || currentFolder.files.length > 0) {
+      // create a list `ul` element
+      let folderList = document.createElement("ul");
+      folderList.setAttribute("class", "content-list");
 
-        // check if the current folder
-        // has any subfolders or files
-        if(currentFolder.subfolders.length > 0 || currentFolder.files.length > 0 ){
+      currentFolder.subfolders.forEach((folder) => {
+        // for each folder inside the current folder
 
+        // create a list item `li`
+        let listItem = document.createElement("li");
 
-            // create a list `ul` element
-            let folderList = document.createElement('ul')
-            folderList.setAttribute('class','content-list')
+        let folderIcon = _.find(icons, { name: "folder-icon" });
 
+        // intert the folder name into the list item `li`
+        listItem.innerHTML = `${folderIcon.svgPath}`;
+        listItem.innerHTML += folder.name;
 
-            currentFolder.subfolders.forEach((folder)=>{// for each folder inside the current folder
+        //append list item to the folderList
+        folderList.appendChild(listItem);
 
-                // create a list item `li`
-                let listItem = document.createElement('li')
+        //append folderList into the commandOutputContainer element.
+        commandOutputContainer.appendChild(folderList);
+      });
 
-                let folderIcon = _.find(icons, {name : 'folder-icon'})
-                console.log(folderIcon)
+      currentFolder.files.forEach((file) => {
+        // for each file inside the current folder
 
-                // intert the folder name into the list item `li`
-                listItem.innerHTML = `${folderIcon.svgPath}`
-                listItem.innerHTML += folder.name
+        // create a list item `li`
+        let listItem = document.createElement("li");
 
-                //append list item to the folderList
-                folderList.appendChild(listItem)
+        let fileIcon = _.find(icons, { name: "file-icon" });
 
-                //append folderList into the commandOutputContainer element.
-                commandOutputContainer.appendChild(folderList)
-            })
+        // intert the file name into the list item `li`
+        listItem.innerHTML = `${fileIcon.svgPath}`;
+        listItem.innerHTML += file.name;
 
+        //append list item to the folderList
+        folderList.appendChild(listItem);
 
-            currentFolder.files.forEach((file)=>{// for each file inside the current folder
-
-                // create a list item `li`
-                let listItem = document.createElement('li')
-
-                let fileIcon = _.find(icons, {name :'file-icon'})
-
-                // intert the file name into the list item `li`
-                listItem.innerHTML = `${fileIcon.svgPath}`
-                listItem.innerHTML += file.name
-
-                //append list item to the folderList
-                folderList.appendChild(listItem)
-
-                //append folderList into the commandOutputContainer element.
-                commandOutputContainer.appendChild(folderList)
-            })
-
-        }else{
-            error(`No content inside the current directory. ☹`)
-        }
-
-    }else{// the user wrote more than the `ls` keyword
-        error(`That command does not exist.☹`)
+        //append folderList into the commandOutputContainer element.
+        commandOutputContainer.appendChild(folderList);
+      });
+    } else {
+      error(`No content inside the current directory. ☹`);
     }
-   
-}
-
-
-/**
- * Removes a file from the current directory.
- * @param {Array} command The user input split into words
- */
-function rm(command){
-
-    // check if the user wrote more after the `rmdir`/`rm` keywords
-    if(command.length > 1){
-        
-        // set name to the words after the `del` keyword
-        let name = _.join(command.slice(1, maxNameSize), ' ')
-
-        
-        // if the user wrote `rm` (they wanted to delete a file)
-        if(command[0] === 'rm'){
-            
-            //find a file with the name specified by the user
-            let fileMatch =  _.find(currentFolder.files,{name:name})
-
-            // if such file was found, remove it
-            if(fileMatch != undefined){
-                let removed = _.remove(currentFolder.files, (file)=>{
-                    return file.name === file.name
-                })
-                commandOutputContainer.innerHTML += `
-                    <p> successfully deleted <b>${name}</b> file. 🚽</p>
-                `
-            }else{
-                error(`No such <b>file</b> was found. To delete a folder instead, use <b>rmdir</b>`)
-            }
-
-
-        }
-
-        
-        // If the user wrote `rmdir` (they want to delete a directory)
-        if (command[0] === 'rmdir'){
-
-            // find a folder with the name specified by the user (name)
-            let folderMatch = _.find(currentFolder.subfolders,{name:name})
-
-            // If such folder is found, remove it
-            if (folderMatch != undefined) {
-                let removed = _.remove(currentFolder.subfolders, (folder)=>{
-                    return folder.name === folderMatch.name
-                })
-                commandOutputContainer.innerHTML += `
-                    <p> successfully deleted <b>${name}</b> directory. 🚽</p>
-                `
-                console.log(removed)
-            }else{// match was not found
-                error(`<b>${name}</b> directory does not exist. 😕`)
-            }
-
-
-        }
-
-    }
+  } else {
+    // the user wrote more than the `ls` keyword
+    error(`That command does not exist.☹`);
+  }
 }
 
 /**
- * Creates a new file in the current directory.
- * @param {Array} command The user input split into words
+ * Removes a file with the specified name from the current directory.
+ * @param {string} fileName The file to remove.
  */
-function touch(command){
+function rm(fileName) {
+  if (fileName.length >= 1) {
+    let name;
 
-    if(command.length > 1){//check user provided a file name
+    if (Array.isArray(fileName)) {
+      name = _.join(fileName, " ");
+    } else {
+      name = fileName;
+    }
 
-        // set fileName to the words after the `touch` keyword
-        let fileName = _.join(command.slice(1, maxNameSize), ' ')
+    //find a file with the name specified by the user
+    let matched = _.find(currentFolder.files, { name: name });
 
-        let newFile = {
-            name:fileName,
+    // if such file was found, remove it
+    if (matched != undefined) {
+      _.remove(currentFolder.files, (file) => file.name === matched.name);
 
-        }
+      commandOutputContainer.innerHTML += `
+                    <p> Successfully deleted <b>${name}</b> file. 🚽</p>
+                `;
+    } else {
+      error(`<b>${name}</b> file was not found.😟`);
+    }
+  } else {
+    error(`File to remove was not provided.😟`);
+  }
+}
 
-        currentFolder.files.push(newFile)
-        commandOutputContainer.innerHTML += `
+/**
+ * Removes the folder with the specified name in the current directory.
+ * @param {string} name The name of the folder to be removed.
+ */
+function rmdir(name) {
+  if (name.length >= 1) {
+    // user provided a folder name
+
+    let folderName;
+
+    if (Array.isArray(name)) {
+      folderName = _.join(name, " ");
+    }
+
+    // find a folder with the name specified by the user
+    let folderMatch = _.find(currentFolder.subfolders, { name: folderName });
+
+    // If such folder is found, remove it
+    if (folderMatch != undefined) {
+      _.remove(
+        currentFolder.subfolders,
+        (folder) => folder.name === folderMatch.name
+      );
+      commandOutputContainer.innerHTML += `
+                <p> successfully deleted <b>${folderMatch.name}</b> folder. 🚽</p>
+            `;
+    } else {
+      // match was not found
+      error(`<b>${folderName}</b> folder does was not found. 😕`);
+    }
+  } else {
+    error(`Folder to remove was not provided.😟`);
+  }
+}
+
+/**
+ * Creates a new file with the specified name in the current directory.
+ * @param {string} name The file to create.
+ */
+function touch(name) {
+  if (name.length >= 1) {
+    //check user provided a file name
+
+    let fileName;
+
+    if (Array.isArray(name)) {
+      fileName = _.join(name, " ");
+    } else {
+      fileName = name;
+    }
+
+    let newFile = {
+      name: fileName,
+    };
+
+    currentFolder.files.push(newFile);
+    commandOutputContainer.innerHTML += `
         <p>Created <b>✨${fileName}✨</b> file.</p>
-        `
-
-    }else{// user only wrote `touch` keyword.
-        error(`No <b>file name</b> was provided 😟.`)
-    }
-    
+        `;
+  } else {
+    // user only wrote `touch` keyword.
+    error(`File to create was not provided.😟`);
+  }
 }
 
 module.exports = {
-    cd,
-    mkdir,
-    touch,
-    ls,
-    rm,
-    printPath
-}
+  cd,
+  mkdir,
+  touch,
+  ls,
+  rm,
+  rmdir,
+  printPath,
+};
