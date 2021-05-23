570191c101fec29417e88178d6233f34881d6d35
Anas Mohamed <amohamed2479@gmail.com>
2021-05-22 21:50:37 -0500

    define the `content` wrap element

---
+++ src/js/bashCofig.js
@@ -0,0 +1,13 @@
+
+/**
+ * All the Bash inputs and outputs are wraped inside this element.
+ */
+let contentWrap = document.createElement('div');
+contentWrap.setAttribute('class', 'bash-mirror-content-wrap')
+
+
+
+
+module.exports = {
+    contentWrap
+}
