15bea82f035a33306542416c5afe22fe56e4c61d
Anas Mohamed <amohamed2479@gmail.com>
2021-05-22 22:04:57 -0500

    create `Bash` constructor
    This is the main constructor that will initiate a new Bash-Mirror instance.

--- src/js/Bash.js
+++ src/js/Bash.js
@@ -1,9 +1,157 @@
+const _ = require("lodash");
+const { contentWrap } = require("./bashCofig");
+const CommandLine = require("./command-line");
+const OutPut = require("./command-output");
+const allCommands = require("./commands.config");
+const { error } = require("./errors");
+const NewCommand = require("./new-command");
+const RunCommands = require("./run-commands");
 
-const _ = require('lodash')
 
 
+/**
+ * Bash-Mirror constructor.
+ */
 module.exports = class Bash {
-    constructor(target, options){
 
+
+    /**
+     * 
+     * @param {HTMLElement} target 
+     * @param {{sign:string}} options 
+     */
+    constructor(target, options) {
+
+
+        /* Check if the developer provided a valid target
+        --------------------*/
+        if (target instanceof Element) {
+            this.element = target;
+        } else {
+            console.error(`Bash: Cannot get a valid 'target' element.`);
+        }
+
+
+
+        /* Check if the developer provided an `options` argument
+        --------------------*/
+        if (options === undefined) {
+            this.commandLineSign = `$`;
+        } else {
+            this.commandLineSign = options.sign;
+        }
+
+
+
+        this.contentWrap = contentWrap;
+
+        this.commandLine = new CommandLine(this.commandLineSign);
+
+        this.contentWrap.appendChild(this.commandLine.element);
+
+        this.element.appendChild(contentWrap);
+
+
+        this.commands = allCommands
+
+        this.commandLine.input.addEventListener("keydown", (e) => {
+
+            if (e.key === "Enter") {
+
+                let userInput = _.words(this.commandLine.input.value);
+                this.commandLine.element.remove();
+
+
+                /*If the user wrote nothing
+                ---------------------*/
+                if (userInput.length === 0) {
+
+                    new OutPut(`<p style="margin-bottom:15px;">${options.sign}</p>`);
+
+                } else {
+
+                    new OutPut(`
+                    <div class="user-input">
+                        <span>${options.sign
+                        }</span>  <span class="command-keyword">${userInput[0]
+                        }</span>
+                        <span>${_.join(userInput.slice(1), " ")}</span>
+                    </div>
+                    `);
+
+                    let findCommand = this.commands.find(
+                        (command) => command.name === userInput[0]
+                    );
+
+                    if (findCommand != undefined) {
+
+                        let args = userInput.slice(1)
+
+                        if (findCommand.args === 0 & args.length >= 1) {
+                            error(`<b>${findCommand.name}</b> does not accept arguments.`)
+                        } else {
+                            findCommand.function(args);
+                            console.log(`The arguments '${args}' were fed into the '${findCommand.name}' command.`);
+                        }
+
+                    } else {
+                        error(`Unknown command ☹`);
+                    }
+                }
+                this.commandLine.input.value = "";
+                this.contentWrap.appendChild(this.commandLine.element);
+                this.commandLine.input.focus();
+            }
+        });
     }
-}
+
+
+    /**
+     * Creates a new Bash-Mirror command.
+     * @param {string} cName Command name
+     * @param {Function} cFunction The function to call.
+     * @param {string} cDescription The command description.
+     * @param {number} cArgs The number of arguments command accepts. Default is 0
+     */
+    newCommand(cName, cFunction, cDescription, cArgs) {
+        let newCommd = new NewCommand(cName, cFunction, cDescription, cArgs)
+        this.commands.push(newCommd)
+        
+    }
+
+
+
+    /**
+     * Creates a new Bash-Mirror output.
+     * @param {string} content For HTML, use template literals
+     */
+    outPut(content) {
+        new OutPut(`${this.commandLineSign} ${content}`);
+        this.contentWrap.appendChild(this.commandLine.element);
+        this.commandLine.input.focus();
+    }
+
+
+
+    /**
+     * Creates a new Bash-Mirror error
+     * @param {string} content For HTML, use template literals
+     */
+    error(content) {
+        error(`${this.commandLineSign} ${content}`);
+        this.contentWrap.appendChild(this.commandLine.element);
+        this.commandLine.input.focus();
+    }
+
+
+    /**
+     * Runs a Bash-Mirror command.
+     * @param {string} name The `name` of the command you want to run.
+     * @param {array} args An array of arguments to feed the command's function.
+     */
+    runCommand(name, args){
+       new RunCommands(name, args)
+       this.contentWrap.appendChild(this.commandLine.element);
+    }
+    
+};
