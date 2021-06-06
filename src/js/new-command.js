45f8a1d9ed7bea8715f3787e1c89338f70dc0147
Anas Mohamed <amohamed2479@gmail.com>
2021-06-05 20:33:11 -0500

    simplify
    since the NewCommand constructor create a command object, It should take an object as parameter instead of a series of parameters that'll be used to construct the object. it makes sence :100:

--- src/js/new-command.js
+++ src/js/new-command.js
@@ -1,61 +1,40 @@
-const commandsInfo = require('./commands/commands.json')
-const allCommands = require('./commands.config')
 const _ = require('lodash')
 
 
 
 module.exports = class NewCommand{
+    
     /**
      * 
-     * @param {string} cName Command name
-     * @param {Function} cFunction The function to invoke.
-     * @param {string} cDescription The command description.
-     * @param {number} cArgs The number of arguments command accepts. Default is 0
+     * @param {{ name:string, function:function, description:string}} command 
+     * @returns Bash-Mirror Command
      */
-    constructor(cName, cFunction, cDescription, cArgs) {
-
-
-
+    constructor(command) {
 
         let newCommand = {}
 
-        if(cName != undefined){
+        if(command.name != undefined){
 
-            let check = _.find(allCommands,{name:cName})
-
-            if(check === undefined){
-                newCommand.name = cName
+            
+                newCommand.name = command.name
 
                 
-                if(cFunction != undefined){
-                    newCommand.function = cFunction
-                }
+                if(command.function != undefined){
+                    newCommand.function = command.function
 
-                if (cDescription != undefined) {
-                    newCommand.description = cDescription
-                    commandsInfo.push({name:cName, useCase:cDescription})
-        
-                }
-
-                if (cArgs != undefined) {
-                    newCommand.args = cArgs
+                    if (command.description != undefined) {
+                        newCommand.description = command.description
+            
+                    }
+    
+                    return newCommand
                 }else{
-                    newCommand.args = 0
+                    console.error(`Cannot get ${command.name}'s function.`);
                 }
-
-                return newCommand
-
-            }else{
-                console.error(this,`Command with same name already exist.`);
-                console.log(allCommands);
-            }
             
         }else{
-            console.log(this, `Cannot get '${cName}' name of new command.`);
-        }
-
-        
-        
+            console.error(`Cannot get new command's name!`);
+        } 
     }
 
 }
