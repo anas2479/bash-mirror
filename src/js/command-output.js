a2a39e60f16e52327fb85ed0946b0879422377a0
Anas Mohamed <amohamed2479@gmail.com>
2021-06-05 20:36:21 -0500

    new parameter
    the Output object takes a new parameter, this will be the element new outputs will be appended to.

--- src/js/command-output.js
+++ src/js/command-output.js
@@ -11,13 +11,13 @@ module.exports = class OutPut{
      * For HTML use Template Literals.
      * @param {string} content 
      */
-    constructor(content){
+    constructor(el,content){
         this.element = document.createElement('div')
         this.element.setAttribute('class','command-output-container')
 
         
         this.element.innerHTML = `${content}`
 
-        contentWrap.appendChild(this.element)
+        el.appendChild(this.element)
     }
 }
